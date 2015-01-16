66d738217f845aedff54677b2eab221c6f209710
Bluek404 <i@bluek404.com>
2015-01-17 01:31:14 +0800

    断点续传支持

--- downloader.go
+++ downloader.go
@@ -26,13 +26,12 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"sync"
 	"time"
 )
 
 var (
 	// 最大线程数量
-	MaxThread = 5
+	MaxThread = 1
 	// 缓冲区大小
 	CacheSize = 1024
 
@@ -101,12 +100,12 @@ type FileDl struct {
 	ID       string // 任务ID
 	StoreDir string // 保存到的目录
 
-	onStart  func(id string)
-	onPause  func(id string)
-	onResume func(id string)
-	onDelete func(id string)
-	onFinish func(id string)
-	onError  func(id string, errCode int, errStr string)
+	onStart  func()
+	onPause  func()
+	onResume func()
+	onDelete func()
+	onFinish func()
+	onError  func(errCode int, errStr string)
 
 	paused bool
 	status Status
@@ -132,8 +131,8 @@ func (f *FileDl) Start() {
 		err := f.openFile(true)
 		if err != nil {
 			f.touchOnError(0, err.Error())
+			return
 		}
-		defer f.File.f.Close()
 
 		if f.File.Size <= 0 {
 			f.File.blockList = append(f.File.blockList, block{0, -1})
@@ -150,27 +149,12 @@ func (f *FileDl) Start() {
 			f.File.blockList[MaxThread-1].End += f.File.Size - f.File.blockList[MaxThread-1].End
 		}
 
-		f.startGetSpeeds()
-		var wg sync.WaitGroup
-		wg.Add(MaxThread)
-		for i := range f.File.blockList {
-			// TODO: 断点续传支持
-			go func(id int) {
-				defer wg.Done()
-
-				err := f.downloadBlock(id)
-				if err != nil {
-
-				}
-			}(i)
-		}
-		wg.Wait()
-		f.paused = true
-		err = os.Remove(f.StoreDir + string(os.PathSeparator) + f.File.Name + ".dl")
+		// 开始下载
+		err = f.download()
 		if err != nil {
 			f.touchOnError(0, err.Error())
+			return
 		}
-		f.touch(f.onFinish)
 	}()
 
 	f.touch(f.onStart)
@@ -180,6 +164,10 @@ func (f *FileDl) Start() {
 // 如果文件已存在则打开文件
 // rewrite为true则直接覆盖文件
 func (f *FileDl) openFile(rewrite bool) error {
+	if f.File.f != nil {
+		// 文件已经打开
+		return nil
+	}
 	dirInfo, err := os.Stat(f.StoreDir)
 	if err != nil {
 		err = os.MkdirAll(f.StoreDir, 0644)
@@ -223,6 +211,39 @@ func (f *FileDl) openFile(rewrite bool) error {
 	return nil
 }
 
+func (f *FileDl) download() error {
+	f.startGetSpeeds()
+
+	ok := make(chan bool, MaxThread)
+	for i := range f.File.blockList {
+		// TODO: 断点续传支持
+		go func(id int) {
+			defer func() {
+				ok <- true
+			}()
+
+			err := f.downloadBlock(id)
+			if err != nil {
+				// TODO: 自动重新下载块
+			}
+		}(i)
+	}
+
+	for i := 0; i < MaxThread; i++ {
+		<-ok
+	}
+	// 检查是否为暂停
+	if !f.paused {
+		f.paused = true
+		err := os.Remove(f.StoreDir + string(os.PathSeparator) + f.File.Name + ".dl")
+		if err != nil {
+			return err
+		}
+		f.touch(f.onFinish)
+	}
+	return nil
+}
+
 // 文件块下载器
 // 根据线程ID获取下载块的起始位置
 func (f *FileDl) downloadBlock(id int) error {
@@ -251,28 +272,31 @@ func (f *FileDl) downloadBlock(id int) error {
 		case f.paused == true:
 			return nil
 		default:
-			n, err := resp.Body.Read(buf)
-			// 将缓冲数据写入硬盘
-			func() {
-				f.File.f.WriteAt(append(buf[:n], []byte{}...), f.File.blockList[id].Begin)
-				byt, err := json.Marshal(f.File.blockList)
-				if err != nil {
-					println(err.Error())
-				}
-				// 保存块的下载信息。用于断点续传
-				err = ioutil.WriteFile(f.StoreDir+string(os.PathSeparator)+f.File.Name+".dl", byt, 0644)
-				if err != nil {
-					println(err.Error())
-				}
+			n, e := resp.Body.Read(buf)
 
-				f.status.Downloaded += int64(len(buf[:n]))
+			func() {
+				// 将缓冲数据写入硬盘
+				f.File.f.WriteAt(buf[:n], f.File.blockList[id].Begin)
 			}()
-			f.File.blockList[id].Begin += int64(len(buf[:n]))
+
+			// 保存块的下载信息。用于断点续传
+			byt, err := json.Marshal(f.File.blockList)
 			if err != nil {
-				if err == io.EOF {
+				return err
+			}
+			err = ioutil.WriteFile(f.StoreDir+string(os.PathSeparator)+f.File.Name+".dl", byt, 0644)
+			if err != nil {
+				return err
+			}
+
+			f.status.Downloaded += int64(len(buf[:n]))
+			f.File.blockList[id].Begin += int64(len(buf[:n]))
+
+			if e != nil {
+				if e == io.EOF {
 					return nil
 				}
-				return err
+				return e
 			}
 		}
 	}
@@ -302,60 +326,87 @@ func (f FileDl) GetStatus() Status {
 }
 
 // 暂停下载
-func (f FileDl) Pause() {
+func (f *FileDl) Pause() {
 	f.paused = true
 	f.touch(f.onPause)
 }
 
 // 继续下载
-func (f FileDl) Resume() {
+func (f *FileDl) Resume() {
 	f.paused = false
+	go func() {
+		err := f.openFile(false)
+		if err != nil {
+			f.touchOnError(0, err.Error())
+			return
+		}
+
+		if f.File.blockList == nil {
+			byt, err := ioutil.ReadFile(f.StoreDir + string(os.PathSeparator) + f.File.Name + ".dl")
+			if err != nil {
+				f.touchOnError(0, err.Error())
+				return
+			}
+
+			err = json.Unmarshal(byt, f.File.blockList)
+			if err != nil {
+				f.touchOnError(0, err.Error())
+				return
+			}
+		}
+
+		err = f.download()
+		if err != nil {
+			f.touchOnError(0, err.Error())
+			return
+		}
+	}()
 	f.touch(f.onResume)
 }
 
 // 任务开始时触发的事件
-func (f *FileDl) OnStart(fn func(id string)) {
+func (f *FileDl) OnStart(fn func()) {
 	f.onStart = fn
 }
 
 // 任务暂停时触发的事件
-func (f *FileDl) OnPause(fn func(id string)) {
+func (f *FileDl) OnPause(fn func()) {
 	f.onPause = fn
 }
 
 // 任务继续时触发的事件
-func (f *FileDl) OnResume(fn func(id string)) {
+func (f *FileDl) OnResume(fn func()) {
 	f.onResume = fn
 }
 
 // 任务删除时触发的事件
-func (f *FileDl) OnDelete(fn func(id string)) {
+func (f *FileDl) OnDelete(fn func()) {
 	f.onDelete = fn
 }
 
 // 任务完成时触发的事件
-func (f *FileDl) OnFinish(fn func(id string)) {
+func (f *FileDl) OnFinish(fn func()) {
 	f.onFinish = fn
 }
 
 // 任务出错时触发的事件
 //
 // errCode为错误码，errStr为错误描述
-func (f *FileDl) OnError(fn func(id string, errCode int, errStr string)) {
+func (f *FileDl) OnError(fn func(errCode int, errStr string)) {
 	f.onError = fn
 }
 
 // 用于触发事件
-func (f FileDl) touch(fn func(id string)) {
+func (f FileDl) touch(fn func()) {
 	if fn != nil {
-		fn(f.ID)
+		go fn()
 	}
 }
 
 // 触发Error事件
 func (f FileDl) touchOnError(errCode int, errStr string) {
 	if f.onError != nil {
-		f.onError(f.ID, errCode, errStr)
+		go f.onError(errCode, errStr)
 	}
 }
 
--- downloader_test.go
+++ downloader_test.go
@@ -11,42 +11,65 @@ import (
 )
 
 func Test_NewFile(t *testing.T) {
-	fileDl, err := NewFileDl("", "http://golangtc.com/static/go/go1.4.1.src.tar.gz", -1, "/tmp", "")
+	fileDl, err := NewFileDl("", "http://packages.linuxdeepin.com/ubuntu/dists/devel/main/binary-amd64/Packages.bz2", -1, "/tmp", "")
 	if err != nil {
 		log.Println(err)
 	}
 
-	exit := make(chan bool)
+	var exit = make(chan bool)
+	var resume = make(chan bool)
+	var pause bool
 	var wg sync.WaitGroup
 	wg.Add(1)
-	fileDl.OnStart(func(id string) {
-		fmt.Println(GetDownloader(id).File.Name, "download started")
+	fileDl.OnStart(func() {
+		fmt.Println(fileDl.File.Name, "download started")
 		for {
+			status := fileDl.GetStatus()
+			var i = float64(status.Downloaded) / float64(fileDl.File.Size) * 50
+			h := strings.Repeat("=", int(i)) + strings.Repeat(" ", 50-int(i))
+
 			select {
 			case <-exit:
-				fmt.Println("\n", fileDl.File.Name, "download finished")
+				fmt.Printf("\r%v/%v [%s] %v byte/s %v", status.Downloaded, fileDl.File.Size, h, 0, "[FINISH]")
+				fmt.Println("\n"+fileDl.File.Name, "download finished")
 				wg.Done()
 			default:
-				time.Sleep(time.Second * 1)
-				status := fileDl.GetStatus()
-				var i = float64(status.Downloaded) / float64(fileDl.File.Size) * 50
-				h := strings.Repeat("=", int(i)) + strings.Repeat(" ", 50-int(i))
-				fmt.Printf("\r%v/%v [%s] %v byte/s     ", status.Downloaded, fileDl.File.Size, h, status.Speeds)
-				os.Stdout.Sync()
+				if !pause {
+					time.Sleep(time.Second * 1)
+					fmt.Printf("\r%v/%v [%s] %v byte/s         ", status.Downloaded, fileDl.File.Size, h, status.Speeds)
+					os.Stdout.Sync()
+				} else {
+					fmt.Printf("\r%v/%v [%s] %v byte/s %v", status.Downloaded, fileDl.File.Size, h, 0, "[PAUSE]")
+					os.Stdout.Sync()
+					<-resume
+					pause = false
+				}
 			}
 		}
 	})
 
-	fileDl.OnFinish(func(id string) {
+	fileDl.OnPause(func() {
+		pause = true
+	})
+
+	fileDl.OnResume(func() {
+		resume <- true
+	})
+
+	fileDl.OnFinish(func() {
 		exit <- true
 	})
 
-	fileDl.OnError(func(id string, errCode int, errStr string) {
-		log.Fatal(GetDownloader(id).File.Name, errStr)
+	fileDl.OnError(func(errCode int, errStr string) {
+		log.Fatal(fileDl.File.Name, errStr)
 	})
 
 	fmt.Printf("%+v\n", fileDl)
 
 	fileDl.Start()
+	time.Sleep(time.Second * 2)
+	fileDl.Pause()
+	time.Sleep(time.Second * 3)
+	fileDl.Resume()
 	wg.Wait()
 }
